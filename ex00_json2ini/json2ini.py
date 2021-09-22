b7664d46c9c18de971a02bc35c92c6c3124e2dd2
Damokless <damokless@protonmail.com>
2021-09-22 16:02:04 +0200

    finish exo00

--- ex00_json2ini/json2ini.py
+++ ex00_json2ini/json2ini.py
@@ -1,3 +1,17 @@
 import json
 import sys
 
+with open(sys.argv[1]) as json_file:
+    data = json.load(json_file)
+    mainKeys = list(data.keys())
+    finalString = ""
+    for i in range(0, len(data.keys())):
+        finalString = finalString + '[' + mainKeys[i] + '] \n'
+        subKeys = list(data[mainKeys[i]])
+        subValues = list(data[mainKeys[i]].values())
+        for j in range(0, len(data[mainKeys[i]])):
+            finalString = finalString + subKeys[j] + '= ' + subValues[j] + '\n'
+    with open('output.ini','w') as output:
+        output.write(finalString)
+
+    print(finalString)
