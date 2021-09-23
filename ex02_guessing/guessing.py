28be8c4a33dd0df347247a55c995779ea37903a8
Damokless <damokless@protonmail.com>
2021-09-23 12:21:51 +0200

    add hint for exo02

--- ex02_guessing/guessing.py
+++ ex02_guessing/guessing.py
@@ -3,6 +3,10 @@ import random
 
 guessingNumber = random.randrange(int(sys.argv[1]), int(sys.argv[2]))
 statusGame = False
+if guessingNumber % 2 == 0:
+    print('Hint : Number is even')
+else:
+    print('Hint : Number is odd')
 
 while statusGame == False :
     userInput = int(input('Guess the number between ' + sys.argv[1] + ' and ' + sys.argv[2] + ':  '))
